3fed3e5439ae832d88180ea5c0f1bd7b799c38b4
Ava St. John <avasaintjohn@gmail.com>
2014-02-18 15:08:09 -0800

    wrappup

--- reverse_string.py
+++ reverse_string.py
@@ -1,1 +1,15 @@
-reverse_string.py
+"""Input a string, return the reverse of that string"""
+
+
+def reverse_string(stringy):
+    reversed_stringy = []
+    for i in range(len(stringy)):
+        temp = stringy[-1 -i]
+        reversed_stringy.append(temp)
+    return ("").join(reversed_stringy)
+
+
+
+stringy = "You like poop"
+
+print reverse_string(stringy)
